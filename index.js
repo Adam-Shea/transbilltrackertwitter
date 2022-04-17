8341b022de422089fe1334fdcc09464566b4833e
Adam <davlyn.sheaa@outlook.com>
2022-04-17 15:46:32 +0100

    now using google sheets to pull data

--- index.js
+++ index.js
@@ -1,109 +1,143 @@
 require('dotenv').config()
+const { GoogleSpreadsheet } = require('google-spreadsheet') //Google Spreadsheet
 const TwitterApi = require('twitter-api-v2').default;
 const axios = require('axios'); //Make rest requests
 const fs = require('fs'); //Read files
-const csv = require('fast-csv');
 
-const client = new TwitterApi({
+const googleSheetId = process.env.GOOGLE_SHEET_ID //Google Sheet ID
+const client = new TwitterApi({ //Twitter Client
     appKey: process.env.APP_KEY,
     appSecret: process.env.APP_SECRET,
     accessToken: process.env.ACCESS_TOKEN,
     accessSecret: process.env.ACCESS_SECRET,
 });
-const apiKey = process.env.API_KEY;
+const apiKeyLegiscan = process.env.API_KEY; //Legiscan api key
+//Pulls from .ENV
+
+
+let saveToGoogleSheets = []; //Global save to sheets var to save on api requests
 
 (async function () {
-    await scrapeData();
-    setInterval(function () { scrapeData() }, 20 * 60000)
+    scrapeData();
+    setTimeout(function () {
+        scrapeData();
+        setTimeout(function () {
+            scrapeData();
+            setTimeout(function () {
+                scrapeData();
+            }, 720 * 60000);
+        }, 240 * 60000);
+    }, 240 * 60000);
+    setInterval(function () {
+        scrapeData();
+        setTimeout(function () {
+            scrapeData();
+            setTimeout(function () {
+                scrapeData();
+                setTimeout(function () {
+                    scrapeData();
+                }, 720 * 60000);
+            }, 240 * 60000);
+        }, 240 * 60000);
+    }, 1440 * 60000)
 })();
 
+
+//Scrape data needed
 async function scrapeData() {
-    csvScrape = false
-    billNumbers = []
-    fs.createReadStream('bills.csv')
-        .pipe(csv.parse({ headers: true }))
-        .on('error', error => console.error(error))
-        .on('data', async row => {
-            if (row.Status == "Active") {
-                await axios.get(`https://api.legiscan.com/?key=${apiKey}&op=getSearch&state=${row['Case Name'].split(' ')[0]}&bill=${row['Case Name'].split(' ')[1]}`).then(async function (response) {
-                    billNumbers.push(response.data.searchresult['0'].bill_id)
-                    fs.writeFileSync('customBills.json', JSON.stringify(billNumbers));
-                })
-            }
-        }).on('end', rowCount => console.log(billNumbers)
-        )
+    watchList = fs.readFileSync('watchList.json');
+    //Sets watchList to empty
+
+    watchListRow = await getGoogleSheetsData(0) //Grabs all data from google sheets to add them to the watchlist
+    updateDataFromSheets(watchListRow)
+    for (const row of watchListRow) {
+        if (!row.legiscan_id) {
+            await axios.get(`https://api.legiscan.com/?key=${apiKeyLegiscan}&op=getSearch&state=${row.bill_id.split(' ')[0]}&bill=${row.bill_id.split(' ')[1]}`).then(async function (response) {
+                watchList.push(response.data.searchresult['0'].bill_id)
+                fs.writeFileSync('watchList.json', JSON.stringify(watchList));
+            })
+        }
+    }
+    //Save legiscan id
+
 
     console.log("Starting scrape")
     //Grab api results for 'transgender'
-    axios.get(`https://api.legiscan.com/?key=${apiKey}&year=3&op=getSearch&state=all&query=action:week+AND+%22transgender%22`).then(function (response) {
-        let customBills = JSON.parse(fs.readFileSync('customBills.json'));
+    await axios.get(`https://api.legiscan.com/?key=${apiKeyLegiscan}&year=3&op=getSearch&state=all&query=action:day+AND+%28%22transgender%22+OR+%28%22biological%22+AND+%sex%22%29%29`).then(async function (response) {
+        let customBills = JSON.parse(fs.readFileSync('watchList.json'));
         //Loop through results
         for (let i = 0; i < customBills.length - 1; i++) {
-            processBill(customBills[i]);
+            await processBill(customBills[i]);
         }
         for (let i = 0; i < Object.keys(response.data.searchresult).length - 1; i++) {
             if (response.data.searchresult[i].relevance > 95) {
                 //For each bill, search for the individual bill data
-                processBill(response.data.searchresult[i].bill_id);
+                await processBill(response.data.searchresult[i].bill_id);
             }
         }
         console.log("Finished Scrape")
         console.log("\n\n\n")
     })
+    addGoogleSheetsRow(saveToGoogleSheets, 0);
 }
 
 async function processBill(bill_id) {
-    await axios.get(`https://api.legiscan.com/?key=${apiKey}&op=getBill&id=${bill_id}`).then(async function (response) {
+
+    await axios.get(`https://api.legiscan.com/?key=${apiKeyLegiscan}&op=getBill&id=${bill_id}`).then(async function (response) {
         //Grab stored data for use in avoiding duplicate tweets
         let rawdata = fs.readFileSync('data.json');
         let bills = JSON.parse(rawdata);
+        let currentBill = "";
+
         let exit = false;
 
         //Has bill already been tweeted?
         for (let i = 0; i < bills.length; i++) {
-            if (bills[i].id == String(response.data.bill.bill_id) && bills[i].historyCount == String(Object.keys(response.data.bill.history).length)) {
-                exit = true;
-                break
+            if (bills[i].id == String(response.data.bill.bill_id)) {
+                currentBill = bills[i];
+                if (bills[i].historyCount == String(Object.keys(response.data.bill.history).length)) {
+                    exit = true;
+                    return false
+                }
             }
         }
-        if (exit == true) {
-            return false
-        }
+
+        const historyId = Object.keys(response.data.bill.history).length - 1;
+        const votesId = Object.keys(response.data.bill.votes).length - 1;
+        const legislature = await getLegislature(response.data.bill.state);
 
         //Generate text intro
-        const intro = `🏳️‍⚧️'${response.data.bill.state} ${response.data.bill.bill_number}'`
-        const link = `\n${response.data.bill.state_link}`
-
-        //Generate title
-        let title = `\n⚖️ ${response.data.bill.title}`
+        const intro = `🏳️‍⚧️⚖️'${response.data.bill.state} ${response.data.bill.bill_number}'`
 
         let status = ''
         switch (response.data.bill.status) {
             case 1:
-                status = `\nStatus : Introduced`
+                status = ` has been introduced by ${response.data.bill.sponsors[0].role}. ${response.data.bill.sponsors[0].last_name}, (${response.data.bill.sponsors[0].party})`
                 break;
             case 2:
-                status = `\nStatus : Engrossed`
+                status = ` has passed the ${response.data.bill.history[historyId].chamber} by a vote of ${response.data.bill.votes[votesId].yea}-${response.data.bill.votes[votesId].nay}-${response.data.bill.votes[votesId].absent} and now moves onto the [Senate/House].`
                 break;
             case 3:
-                status = `\nStatus : Enrolled`
+                status = ` has passed the ${response.data.bill.history[historyId].chamber} by a vote of ${response.data.bill.votes[votesId].yea}-${response.data.bill.votes[votesId].nay}-${response.data.bill.votes[votesId].absent} and moves to the desk of ${legislature.Governor}`
                 break;
             case 4:
-                status = `\nStatus : Passed`
+                status = ` has been signed by ${legislature.Governor} `
                 break;
             case 5:
-                status = `\nStatus : Vetoed`
+                status = ` has been vetoed by ${legislature.Governor}`
                 break;
             case 6:
-                status = `\nStatus : Failed`
+                status = ` Failed`
                 break;
         }
 
-        //Generate description
-        let description = response.data.bill.description
+        //Generate title
+        let title = `\n${currentBill.category}, ${status}`
 
-        //Generate history
-        let history = `\n🗒️ ${response.data.bill.history[Object.keys(response.data.bill.history).length - 1].action}`
+        //Generate description
+        let description = "\n" + currentBill.description
+
+        const link = `${response.data.bill.state_link}`
 
         //Write relevant data about bill to json
         //Create JSON data
@@ -111,11 +145,8 @@ async function processBill(bill_id) {
         hasDescription = false
         for (let i = 0; i < bills.length; i++) {
             if (bills[i].id == String(response.data.bill.bill_id)) {
-                storeData = {
-                    status: String(response.data.bill.status),
-                    historyCount: String(Object.keys(response.data.bill.history).length)
-                }
-                bills[i] = storeData
+                bills[i].status = String(response.data.bill.status)
+                bills[i].historyCount = String(Object.keys(response.data.bill.history).length)
                 billExists = true
                 if (bills[i].description) {
                     hasDescription = true
@@ -133,16 +164,91 @@ async function processBill(bill_id) {
             }
             //Write data
             bills.push(storeData)
+
+            storeData = {
+                "legiscan_id": String(response.data.bill.bill_id),
+                "bill_id": String(response.data.bill.state) + " " + String(response.data.bill.bill_number),
+                "link": String(response.data.bill.state_link)
+            }
+            saveToGoogleSheets.push(storeData)
         }
         fs.writeFileSync('data.json', JSON.stringify(bills));
 
         //Tweet
-        console.log((intro + history + description + status + link))
-        if (hasDescription) {
-            const tweetData = (intro + title + history).match(/.{1,275}/g).push(link)
-            //client.readWrite.v1.tweetThread(tweetData)
-        } else {
+        const tweetData = chunkSubstr((intro + title + description), 275)
+        tweetData.push(`${legislature.House}\n${legislature.Senate}\n${legislature.Governor}\n${legislature['Representative Contact Link']}`);
+        tweetData.push(link);
+        console.log(tweetData)
+        client.readWrite.v1.tweetThread(tweetData)
 
-        }
     })
 }
+
+
+//Get google sheets data
+async function getGoogleSheetsData(page_id) {
+    const doc = new GoogleSpreadsheet(googleSheetId)
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+
+    await doc.loadInfo() // loads document properties and worksheets
+
+    const sheet = doc.sheetsByIndex[page_id]
+    const rows = await sheet.getRows({ offset: 0, /*limit:5*/ })
+    return rows;
+
+}
+
+//Write google sheets data
+async function addGoogleSheetsRow(row, page_id) {
+    const doc = new GoogleSpreadsheet(googleSheetId)
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+
+    await doc.loadInfo() // loads document properties and worksheets
+
+    const sheet = doc.sheetsByIndex[page_id]
+
+    const data = await sheet.addRows(row);
+    return data
+
+}
+
+//Update data.json from google sheets
+function updateDataFromSheets(sheetsData) {
+    let data = JSON.parse(fs.readFileSync('data.json'));
+    for (const row of sheetsData) {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id == String(row.legiscan_id)) {
+                data[i].description = row.description
+                data[i].category = row.category
+            }
+        }
+    }
+    fs.writeFileSync('data.json', JSON.stringify(data));
+}
+
+async function getLegislature(id) {
+    const legislatures = await getGoogleSheetsData(1);
+    for (let i = 0; i < 50; i++) {
+        if (legislatures[i].Short == id) {
+            return legislatures[i]
+        }
+    }
+    return { "House": "", "Senate": "", "Governor": "", "Representative Contact Link": "", "Short": "", "State": "" }
+}
+
+function chunkSubstr(str, size) {
+    const numChunks = Math.ceil(str.length / size)
+    const chunks = new Array(numChunks)
+
+    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+        chunks[i] = str.substr(o, size) + "..."
+    }
+
+    return chunks
+};
